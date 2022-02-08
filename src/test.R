9b6bbf2588210e7f10bfaa8ebfba946427c40535
Jan- Erik Thrane <jet@niva.no>
2022-02-08 16:38:49 +0100

    n

--- src/test.R
+++ src/test.R
@@ -4,6 +4,12 @@ library(openxlsx)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+
+SG <- rgb(0,92,230, max = 255)
+G <- rgb(0,144,54, max = 255)
+M <- rgb(255, 236, 0, max = 255)
+D <- rgb(242, 148,0, max = 255)
+SD <- rgb(236,28,36, max = 255)
 ############################################################################
 
 d.askjum <-read.xlsx("2022-02-07_Askjumelva nedre.xlsx", detectDates = T) 
@@ -77,7 +83,6 @@ sum(complete.cases(d$NO3.comb))
 sum(complete.cases(d$N.NO3))
 sum(complete.cases(d$N.SNOX))
 
-
 ### Sjekk vanntype-parametere ###
 bwplot(FARGE ~ factor(year) | Vannlokalitetsnavn, data = filter(d, year > 2009))
 boxplot(FARGE ~  Vannlokalitetsnavn, data = filter(d, year > 2009), log = "y")
@@ -91,7 +96,10 @@ d %>% group_by(Vannlokalitetsnavn) %>%
 boxplot(TOC ~  Vannlokalitetsnavn, data = filter(d, year > 2009), log = "y")
 boxplot(CA~  Vannlokalitetsnavn, data = filter(d, year > 2009), log = "y")
 
+#####################
 ### Klassegrenser ###
+#####################
+
 # TOTP i moderat kalkrike og kalkrike, klare elver i lavland
 tp1 = data.frame(vanntype = "mod_kalkrik_klar_lavland", referanse = 9, SGG = 15, GM = 25, MD = 38, DSD = 65)
 # TOTP i moderat kalkrik, skog (kalkrik, skog finnes ikke - går under denne)
@@ -100,10 +108,69 @@ tp2 = data.frame(vanntype = "mod_kalkrik_klar_skog", referanse = 6, SGG = 11, GM
 tp3 = data.frame(vanntype = "mod_kalkrik_humøs_lavland", referanse =11, SGG = 20, GM = 29, MD = 58, DSD = 98)
 # TOTP humøs, moderat kalkrik, skog
 tp4 = data.frame(vanntype = "mod_kalkrik_humøs_skog", referanse =9, SGG = 17, GM = 24, MD = 45, DSD = 83)
-
 tp.klassegrenser <- rbind(tp1, tp2, tp3, tp4)
 
+#############################################
+### Plott for hver elv og TOTP ##############
+#############################################
+unique(d$Vannlokalitetsnavn)
 
+#### Vigga ###
+# Antall observasjoner pr. år av TotP
+with(filter(d, Vannlokalitetsnavn == "Røykenvik" & complete.cases(P.TOT)), table(year))
+
+# Definerer year som faktor med alle år for plott
+d.vig <- filter(d, Vannlokalitetsnavn == "Røykenvik")
+d.vig$year <- factor(d.vig$year, levels = 2012:2021)
+
+d.vig.mean <- d.vig %>% 
+  group_by(year) %>%
+  summarize_all(mean, na.rm = T) %>% 
+  data.frame()
+
+
+### TOTP og TOTN ###
+png("totP og totN Vigga.png", res = 1000, height = 7.2, width = 7.5, units = "in")
+par(mfrow = c(2,1), mar = c(2,5,2,2), oma = c(4,2,2,2))
+# Boxplot: om man ønsker å ta med de gamle dataene (94-96) så bruk d.vig
+boxplot(P.TOT ~ year, data = d.vig, log = "y", axes = F, border = NA,
+        ylab = "Total fosfor (µg P/L)")
+rect(xleft = -1, xright = 50, ybottom = 0.1, ytop = 15, col = SG, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 15, ytop = 25, col = G, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 25, ytop = 38, col = M, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 38, ytop = 65, col = D, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 65, ytop = 200, col = SD, border = NA)
+boxplot(P.TOT ~ year, data = d.vig, log = "y", add = T, border = "grey51", las = 2)
+
+# Add gjennomsnitt som punkt
+points(P.TOT ~ year, data = d.vig.mean, col = 1, pch = "*", cex = 1.5, log = "y")
+
+# TOTN
+with(filter(d, Vannlokalitetsnavn == "Røykenvik" & complete.cases(N.TOT)), table(year))
+boxplot(N.TOT ~ year, data = d.vig, log = "y", axes = F, ylim  = c(250, 8000), border = "grey31",
+        ylab = "Total nitrogen (µg N/L)")
+rect(xleft = -1, xright = 50, ybottom = 0.1, ytop = 425, col = SG, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 425, ytop = 675, col = G, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 675, ytop = 950, col = M, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 950, ytop = 1425, col = D, border = NA)
+rect(xleft = -1, xright = 50, ybottom = 1425, ytop = 10000, col = SD, border = NA)
+boxplot(N.TOT ~ year, data = d.vig, log = "y",add = T, border = "grey51", las = 2)
+points(N.TOT  ~ year, data = d.vig.mean, col = 1, pch = "*", cex = 1.5)
+dev.off()
+
+### Fosfat og nitrat ###
+d.vig2 <- filter(d, Vannlokalitetsnavn == "Røykenvik" & year >= 2012)
+d.vig2year <- factor(d.vig$year, levels = 2014:2021)
+
+png("PO4 og NO3 Vigga.png", res = 1000, height = 7.2, width = 7.5, units = "in")
+par(mfrow = c(2,1), mar = c(2,5,2,2), oma = c(4,2,2,2))
+# Fosfat
+with(filter(d, Vannlokalitetsnavn == "Røykenvik" & complete.cases(PO4.comb)), table(year))
+boxplot(PO4.comb ~ year, data = d.vig2, log = "y")
+
+# Nitrat
+with(filter(d, Vannlokalitetsnavn == "Røykenvik" & complete.cases(NO3.comb)), table(year))
+boxplot(NO3.comb ~ year, data = d.vig2, log = "y")
 
 
 
